6eeded8905eec45a3e492154b95fc3d8302e8014
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-12-26 23:37:51 +0530

    books data from redux action

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,70 +1,67 @@
 import React, { useEffect, useState } from 'react'
-import { SEARCH_ENDPOINT } from '../utils/util'
 import Row from 'react-bootstrap/Row';
 import Spinner from 'react-bootstrap/Spinner';
 import Col from 'react-bootstrap/Col';
 import Product from './Product';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../store/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook, removeBook, savedTech } from '../store/cartSlice';
+import { getBooks, resetBooks } from '../store/bookProductsSlice';
 
 
 export default function Products() {
-  const [selectTech, setSelectTech] = useState('all');
+  const alreadySelected = useSelector(state => state.cart.selectedTechnology);
+  const bookProducts = useSelector(state => state.bookProducts);
+  const [selectTech, setSelectTech] = useState(alreadySelected || 'all');
   const dispatch = useDispatch();
-  const [products, setProducts] = useState({
-    allBooks: []
-  });
 
   useEffect(() => {
-    const getAllBooks = async () => {
-      const noOfApiCalls = [1, 2, 3, 4];
-      await Promise.all(
-        noOfApiCalls.map(async (id) => {
-          const res = await fetch(`${SEARCH_ENDPOINT}/${selectTech}?page=${id}`)
-          const response = await res.json();
-          setProducts(products => ({
-            response,
-            allBooks: [...products.allBooks, ...response?.books]
-          }));
-        })
-      )
-    }
-    getAllBooks();
-  }, [selectTech])
+    dispatch(getBooks(selectTech));
+  }, [selectTech, dispatch])
+
+
 
   const addToCartFn = (book) => {
     // dispatch an addBook action
-    dispatch(addBook(book))
-    console.log(book);
+    dispatch(addBook(book));
   }
+
+  const removeFromCartFn = (book) => {
+    dispatch(removeBook(book));
+  }
+
+  console.log('products: ', bookProducts)
 
   return (
     <Col justify-content='center'>
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         <select onChange={(e) => {
           setSelectTech(e.target.value);
-          setProducts({
-            ...products,
-            allBooks: []
-          });
-
-        }}>
+          dispatch(resetBooks());
+          dispatch(savedTech(e.target.value));
+        }}
+          value={selectTech}
+          style={{ width: '40%' }}
+        >
           <option value='all'>All</option>
           <option value='JavaScript'>Javascript</option>
           <option value='Java'>Java</option>
           <option value='CSS'>CSS</option>
           <option value='mongodb'>Mongodb</option>
           <option value='Zookeeper'>Zookeeper</option>
+          <option value='python'>Python</option>
+          <option value='ruby'>Ruby</option>
+          <option value='php'>PHP</option>
+          <option value='nodejs'>Nodejs</option>
         </select>
-        {/* <input type='text' placeholder='Enter technology name to search' onChange={(e) => setSelectTech(e.target.name)}/> */}
       </div>
-      {products?.allBooks?.length < 1 && <Spinner animation="grow" variant="primary" />}
-      <Row xs={1} md={5} className="g-4">
-        {products?.allBooks?.map((book, i) =>
+      {bookProducts?.allBooks?.length < 1 && <Spinner animation="grow" variant="primary" />}
+      <Row xs={1} md={5} className="g-4" style={{ justifyContent: 'center' }}>
+        {bookProducts?.allBooks?.map((book, i) =>
           <Product
             book={book}
             key={book.isbn13 + '-' + i}
             addToCartFn={addToCartFn}
+            removeFromCartFn={removeFromCartFn}
           />
         )}
       </Row>
